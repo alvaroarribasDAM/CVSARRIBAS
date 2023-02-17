dc7aa5ba868bcec8d172c5fbf4d36626ec2f42f7
dam <dam@203P20>
2023-02-17 09:48:58 +0100

    Primer commit del examen.

---
+++ src/cvsarribas/Clase1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cvsarribas;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    public static void main(String[] args) {
+        System.out.println("Álvaro Arribas Porres");
+        System.out.println("Segunda línea de código.");
+    }
+}
